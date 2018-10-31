ddba49c439ee540ec69b4f3253247193dd0bf0f0
ehlinashin <ehlinashin@yandex.ru>
2018-10-31 12:43:34 +0300

    AS-3 Fix

--- src/main/java/demo/ComputeEngineVariableType.java
+++ src/main/java/demo/ComputeEngineVariableType.java
@@ -2,5 +2,6 @@ package demo;
 
 public enum ComputeEngineVariableType {
     BOOLEAN,
-    LIST
+    LIST,
+    INTEGER
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -6,6 +6,5 @@ public class Main {
     public static void main(String[] args) {
         ValuatedComputeEngineVariable var = new ComputeEngineVariable("var", ComputeEngineVariableType.LIST, Arrays.asList(1, 2, 3));
         System.out.println(var.invokeOperation(ComputeEngineVariableOperations.SIZE));
-        System.out.println(var.invokeOperation(ComputeEngineVariableOperations.TO_STRING));
     }
 }
